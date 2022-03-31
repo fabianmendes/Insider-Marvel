e6a6aa081e546e51cac7336d39143cf4b0f01239
V. Fabian T. Mendes C <31230743+fabianmendes@users.noreply.github.com>
2022-03-31 16:02:09 +0000

    Creation of first steps for Orbiter plushing web site from scrapping.

--- main.py
+++ main.py
@@ -4,6 +4,12 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.common.keys import Keys
+# For Scrapping.----------------------------------
+import requests
+from bs4 import BeautifulSoup
+import html
+#from lxml import etree
+#------------------------------------------------
 
 chrome_options = Options()
 chrome_options.add_argument('--no-sandbox')
@@ -11,38 +17,35 @@ chrome_options.add_argument('--disable-dev-shm-usage')
 
 driver = webdriver.Chrome(options=chrome_options)
 driver.implicitly_wait(40)  # seconds
-driver.get(	"https://www.marvel.com/signin?referer=https%3A%2F%2Fwww.marvel.com%2Finsider")
+paggina = "https://orbitacomunicacion.com/"
+driver.get(paggina + "entra/")
 
 #Detecting items:
 browser = driver
-username ="email"
-password ="contrasena"
-
+username ="orbiter"
+password ="@fc#Db)4Ku*S9"
 
 WebDriverWait(driver, 20).until(
-        expected_conditions.presence_of_element_located((By.ID, 'disneyid-iframe')))
-#framing = driver.find_element_by_id('disney-wrapper')
-
-
-a = driver.find_elements_by_tag_name("div")#[2]
-d = driver.find_element_by_id('disneyid-wrapper')
-driver.switch_to.frame("disneyid-iframe")  # disneyid-wrapper
-print("\n", a,   "\n", d
-      )
-
+    expected_conditions.
+        presence_of_element_located(
+        (By.ID, 'user_login')))
+        # //*[@id="user_login"]
 yeah = driver.find_element_by_xpath(
-    "/html/body/div[1]/div/div/section/section/form/section/div[1]/div/label/span[2]/input"
+    '//*[@id="user_login"]'
     )
-#yeah2= driver.find_elements_by_class_name("input-wrapper")[1]
-# print(str(len(yeah2)), yeah2)
-yeah.click()
-
-yeah.send_keys(username)  # email
+yeah.send_keys(username)
 hell = driver.find_element_by_xpath(
-    '//*[@id="did-ui-view"]/div/section/section/form/section/div[2]/div/label/span[2]/input')
+       '//*[@id="user_pass"]'
+        )
 hell.click()
 hell.send_keys(password)
 hell.send_keys(Keys.ENTER)
 
-# DONE. So far here. Now let's rocking it with the activities!.
+WebDriverWait(driver, 20).until(
+    expected_conditions.
+        presence_of_element_located((By.XPATH,
+                                     '//*[@id="editor"]/div[1]/div[1]/div[2]/div[2]')))
+
+
+# DONE. We are in! Now let's write.
 # ------------------
